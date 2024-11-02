04251a15687d4342ad6bc138693519bab87190b0
Admin <raevgeorgii.kric@gmail.com>
2024-11-03 01:09:57 +0300

    обавил новую метрику схожести, добавил бота, почистил ненужные файлы

--- my_utils.py
+++ my_utils.py
@@ -1,10 +1,12 @@
 import json
 import torch
 from tqdm.auto import tqdm
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 import numpy as np
+import nltk
+import re
+import pymorphy3
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 def split_document(json_file, tokenizer, max_context_length=450, overlap=100):
     '''
@@ -16,7 +18,6 @@ def split_document(json_file, tokenizer, max_context_length=450, overlap=100):
     :param overlap: длина перекрывания (тоже в токенах)
     :return: Список из title + content, который поделен на чанки
     '''
-
     # читаем файл, получаем список
     with open(json_file, 'r', encoding='utf-8') as f:
         data = json.load(f)['data']
@@ -41,58 +42,129 @@ def split_document(json_file, tokenizer, max_context_length=450, overlap=100):
             data[i]['content'] = new_content
     return data
 
-def relevant_chunk_finder(data_list, question, tokenizer, model, embedding_type='last'):
+
+def cos_similarities(embeddings, q_embedding):
+    similarities = []
+    for i in range(len(embeddings)):
+        similarity = cosine_similarity(q_embedding, embeddings[i]).mean()
+        similarities.append(similarity)
+    return similarities
+
+
+def euclidean_similarities(embeddings, q_embedding):
+    similarities = []
+    for i in range(len(embeddings)):
+        similarity = euclidean_distances(q_embedding, embeddings[i]).mean()
+        similarities.append(similarity)
+    return similarities
+
+
+def cleaning(content, ru_stops, n):
+    morph = pymorphy3.MorphAnalyzer()
+    text = content.lower()
+    text = re.sub(r'[^\w\s]', '', text)
+
+    words = text.split()
+    processed_words = []
+    for word in words:
+        if word not in ru_stops:
+            lemma = morph.parse(word)[0].normal_form
+            processed_words.append(lemma)
+    n_grams = set()
+    for i in range(1, n + 1):
+        i_gram = set(nltk.ngrams(processed_words, i))
+        n_grams = n_grams.union(i_gram)
+    return set(n_grams)
+
+def precision(context, question, n=3):
+    # нормализация нашего чудесного текста
+    with open('stops.txt', 'r', encoding='utf-8') as f:
+        ru_stops = set(f.read().splitlines())
+    set_question = cleaning(question, ru_stops, n)
+    set_context = cleaning(context, ru_stops, n)
+    score = len(set_context.intersection(set_question))/len(set_question)
+    return score
+
+
+def get_embeddings(text, tokenizer, model):
+    model.eval()
+    tokens = tokenizer.encode_plus(text, return_tensors='pt')
+    tokens = {k: v.to(model.device) for k, v in tokens.items()}
+    decoder_input_ids = torch.tensor([[tokenizer.pad_token_id]]).to(model.device)
+
+    with torch.no_grad():
+        outputs = model(input_ids=tokens['input_ids'], decoder_input_ids=decoder_input_ids,
+                        output_hidden_states=True)
+
+    last_hidden_state = outputs.encoder_last_hidden_state
+    embedding = last_hidden_state.mean(dim=1).squeeze().cpu().tolist()
+    return embedding
+
+
+def relevant_chunk_finder(data_list,
+                          question,
+                          tokenizer,
+                          model,
+                          similarity_type='precision'):
+    '''
+    Выбирает наиболее релевантный раздел, основываясь на метрике similarity_type
+
+    :param data_list: List[dict{title, content, ...}] - список заголовков с контекстами и их векторными представлениями
+    :param question: вопрос в формате str
+    :param tokenizer: tokenizer
+    :param model: model
+    :param similarity_type: способ сравнения схожести вектора вопроса и контекста ['precision', 'cosin', 'euclidean']
+    :return: раздел, который ближе всего к вопросу в формате List[str]
+    '''
+    model.eval()
     # если нет эмбеддингов, то создаем их
-    if 'embeddings' not in list(data_list[0].keys()):
+    if 'embeddings' in list(data_list[0].keys()):
         data = data_list.copy()
-        model.eval()
+    else:
+        print('no embeddings, generating...')
+        data = data_list.copy()
         for i in tqdm(range(len(data))):
             content = data[i]['content']
             data[i]['embeddings'] = []
             for text in content:
-                tokens = tokenizer.encode_plus(text, return_tensors='pt')
-                tokens = {k: v.to(model.device) for k, v in tokens.items()}
-                decoder_input_ids = torch.tensor([[tokenizer.pad_token_id]]).to(device)
+                embedding = get_embeddings(text, tokenizer, model)
+                data[i]['embeddings'].append(embedding)
 
-                if embedding_type == 'last':
-                    with torch.no_grad():
-                        outputs = model(input_ids=tokens['input_ids'], decoder_input_ids=decoder_input_ids,
-                                        output_hidden_states=True)
-
-                    last_hidden_state = outputs.encoder_last_hidden_state
-                    embedding = last_hidden_state.mean(dim=1).squeeze().cpu().tolist()
-                    data[i]['embeddings'].append(embedding)
+    # если precision, то схожесть по множеству слов, иначе схожесть эмбеддингов
+    if similarity_type == 'precision':
+        # разные n-грамы, потому что если есть сходство для 3х, то это лучше, чем по 1, кмк
+        similarities = []
+        for i in range(len(data)):
+            max_similarity = 0
+            for j in range(len(data[i]['content'])):
+                content = data[i]['content'][j]
+                similarity = precision(context=content, question=question)
+                if max_similarity < similarity:
+                    max_similarity = similarity
+            similarities.append(max_similarity)
     else:
-        data = data_list
-
-    # получаем эмбеддинг вопроса
-    q_tokens = tokenizer.encode_plus(question.replace('?', ''), return_tensors='pt')
-    q_tokens = {k: v.to(model.device) for k, v in q_tokens.items()}
-    decoder_input_ids = torch.tensor([[tokenizer.pad_token_id]]).to(device)
-
-    if embedding_type == 'last':
-        with torch.no_grad():
-            outputs = model(input_ids=q_tokens['input_ids'], decoder_input_ids=decoder_input_ids,
-                            output_hidden_states=True)
-
-        last_hidden_state = outputs.encoder_last_hidden_state
-        q_embedding = last_hidden_state.mean(dim=1).squeeze().cpu().tolist()
-
-    # находим среднее косинусное расстояние для каждого раздела до вопроса
-    similarities = []
-    for i in range(len(data)):
-        embeddings = data[i]['embeddings']
-        similarity = cosine_similarity(q_embedding, embeddings).mean()
-        similarities.append(similarity)
+        # получаем эмбеддинг вопроса
+        print('Question embedding generating!')
+        q_embedding = get_embeddings(question.replace('?', ''), tokenizer, model)
+        print('DONE')
+        embeddings = [data[i]['embeddings'] for i in range(len(data))]
+        # находим среднее косинусное расстояние для каждого раздела до вопроса
+        if similarity_type == 'cosin':
+            similarities = cos_similarities(embeddings, q_embedding)
+        elif similarity_type == 'euclidean':
+            similarities = euclidean_similarities(embeddings, q_embedding)
 
     # берем самые крутые тексты (мб и 1 текст)
     most_relevant = np.argmax(similarities)
-    return data[most_relevant]['content']
+    return data[most_relevant]['content'], similarities
+
 
 def get_answer(contexts, question, tokenizer, model):
+    model.eval()
     QA_PROMPT = "Сгенерируй ответ на вопрос по тексту. Текст: '{context}'. Вопрос: '{question}'."
     confidence_scores = []
     answers_tokens = []
+    print('Start finding!')
     for context in contexts:
         prompt = QA_PROMPT.format(context=context, question=question)
         prompt_tokens = tokenizer.encode_plus(prompt, return_tensors='pt')
@@ -109,8 +181,7 @@ def get_answer(contexts, question, tokenizer, model):
                                      top_p=0.9,
                                      top_k=50,
                                      output_scores=True,
-                                     return_dict_in_generate=True,
-                                     early_stopping=True)
+                                     return_dict_in_generate=True)
 
         # Получаем токены и лог-вероятности
         generated_tokens = outputs.sequences[0]
@@ -124,8 +195,10 @@ def get_answer(contexts, question, tokenizer, model):
         confidence_score = sum(predicted_probabilities) / len(predicted_probabilities)
         confidence_scores.append(confidence_score)
         answers_tokens.append(generated_tokens)
-
+    print('DONE')
     best_idx = np.argmax(confidence_scores)
+    print(contexts[best_idx])
+    print(np.max(confidence_scores))
     answer = tokenizer.decode(answers_tokens[best_idx], skip_special_tokens=True)
     return answer
 
---
+++ tg_bot.py
@@ -0,0 +1,113 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
+import os
+from dotenv import load_dotenv
+from my_utils import *
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+import torch
+
+# Словарь с вариантами файлов для кнопок
+file_options = {
+    "file1": "Naumen Desk",
+    "file2": "Service registratsii",
+}
+
+# Хранилище данных пользователя
+user_data = {}
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Доброго времени суток! Здесь вы можете задать вопросы по разным документациям, и, возможно, я смогу на них ответить, но ничего не гарантирую.\n Для того, чтобы задать новый вопрос, введите команду /new_question'
+    )
+
+
+# Команда /new_question для нового вопроса
+async def new_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [[InlineKeyboardButton(name, callback_data=key)] for key, name in file_options.items()]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Выберите вариант файла:",
+        reply_markup=reply_markup
+    )
+
+
+# Обработка выбора кнопки
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    # Сохранить выбранный файл
+    selected_option = query.data
+    user_data[query.from_user.id] = {"file": file_options[selected_option]}
+
+    await query.edit_message_text(text=f"Вы выбрали: {file_options[selected_option]}. Теперь задайте свой вопрос.")
+
+
+# Обработка текста (ожидание вопроса от пользователя)
+async def question_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+
+    # Проверяем, выбрал ли пользователь файл
+    if user_id in user_data:
+        user_question = update.message.text
+        selected_file = user_data[user_id]["file"]
+
+        # Сохраняем вопрос
+        user_data[user_id]["question"] = user_question
+
+        # Отправляем подтверждение
+        await update.message.reply_text(f"Вы задали вопрос: '{user_question}' для файла '{selected_file}'.\n Это може занять пару минут...")
+
+        # открываем файл, который выбрал пользователь и передаем его с вопросом в finder:
+        file_name = selected_file.replace(' ', '_')
+        file_name = f'{file_name}_splited_emb.json'
+        path_to_file = os.path.join(context.bot_data['path_to_docs_dir'], file_name)
+        with open(path_to_file, 'r', encoding='utf-8') as f:
+            data_list = json.load(f)['data']
+        relevant_chunks, _ = relevant_chunk_finder(data_list=data_list,
+                                                question=user_question,
+                                                tokenizer=context.bot_data['tokenizer'],
+                                                model=context.bot_data['model'])
+        # теперь генерируем ответ
+        answer = get_answer(contexts=relevant_chunks,
+                            question=user_question,
+                            tokenizer=context.bot_data['tokenizer'],
+                            model=context.bot_data['model'])
+
+        # Жуткий костыль, мне очень стыдно
+        if answer == user_question:
+            await update.message.reply_text(text='Пожалуйста, перефразируйте вопрос, так как я не смог найти ответ на ваш вопрос.')
+        else:
+            await update.message.reply_text(text=f'Ответ на ваш вопрос:\n{answer}\n')
+    else:
+        await update.message.reply_text("Сначала выберите вариант файла с помощью команды /new_question")
+
+
+# Основной запуск бота
+if __name__ == '__main__':
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    # загрузка токена
+    load_dotenv('token.env')
+    TOKEN = os.getenv("TG_TOKEN")
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    # загрузка модели и токенизатор и пути к файлам
+    if os.path.exists('./t5_model/'):
+        saved_checkpoint = './t5_model/'
+    else:
+        saved_checkpoint = 'hivaze/AAQG-QA-QG-FRED-T5-1.7B'
+    tokenizer = AutoTokenizer.from_pretrained(saved_checkpoint)
+    model = T5ForConditionalGeneration.from_pretrained(saved_checkpoint)
+    app.bot_data['model'] = model.to(device)
+    app.bot_data['tokenizer'] = tokenizer
+    app.bot_data['path_to_docs_dir'] = 'docs/docs_json/'
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("new_question", new_question))
+    app.add_handler(CallbackQueryHandler(button_handler))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, question_handler))
+
+    print("Бот запущен...")
+    app.run_polling()
